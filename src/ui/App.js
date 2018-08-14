70692a92e06f5812523b389a4712a1bddbf2c6a2
svvolynkin@gmail.com <sergey.volynkin@akvelon.com>
2018-08-14 20:49:02 +0300

    AppBar added

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import WordCountPage from './pages/WordCountPage/WordCountPage'
+import AppBar from './SimpleAppBar'
 
 class App extends Component {
   render() {
     return (
       <div>
-        AppBar
+        <AppBar/>
         <WordCountPage/>
           - Paper
             - FilesSelectionFragment then FilesParsingFragment then HistogramFragment
